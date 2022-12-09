93b401edd0beab4975257543a60735002816aeb6
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2022-12-09 10:39:43 -0500

    Updated login-view fetch

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -13,7 +13,7 @@ export const LoginView = ({ onLoggedIn }) => {
       secret: password
     };
 
-    fetch("https://justin-myflixdb.herokuapp.com/users", {
+    fetch("https://justin-myflixdb.herokuapp.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
