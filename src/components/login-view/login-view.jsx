65864fed2983f689d27d5434b4872feb0db643e1
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2023-01-09 22:18:31 -0500

    Updated login-view data fields

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -11,8 +11,8 @@ export const LoginView = ({ onLoggedIn }) => {
     event.preventDefault();
 
     const data = {
-      access: username,
-      secret: password
+      Username: username,
+      Password: password
     };
 
     fetch("https://justin-myflixdb.herokuapp.com/login", {
